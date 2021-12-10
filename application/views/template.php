21b71c0375565c62c7049976d02e1976542deb8e
Sulkhi <sulkhi25@gmail.co>
2021-12-10 18:33:32 +0700

    edit sedikit

--- application/views/template.php
+++ application/views/template.php
@@ -449,7 +449,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Tambahkan Jenis Bencana</h5>
+                    <h5 class="modal-title">Jenis Bencana</h5>
                     <a href="#" class="close" data-dismiss="modal" aria-label="Close">
                         <em class="icon ni ni-cross"></em>
                     </a>
